59f86b1a3764c8b4da4bb492ff6314eaa9d4af06
PavelKosyrev <65193274+PavelKosyrev@users.noreply.github.com>
2020-09-14 23:49:13 +0300

    работа над ошибками

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,7 +1,12 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
+var siteList = document.querySelector('.site-list');
 
       navMain.classList.remove('main-nav--nojs');
+
+      siteList.classList.add('site-list--js');
+
+      navToggle.classList.add('main-nav__toggle--js');
 
       navToggle.addEventListener('click', function() {
         if (navMain.classList.contains('main-nav--closed')) {
